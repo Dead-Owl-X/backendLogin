a612a6a5ac736c967c5e0abca60493da8a92e499
Jesús Cortés <sergio3cortes@gmail.com>
2025-04-29 11:26:26 -0600

    changes

--- users/user.service.js
+++ users/user.service.js
@@ -18,7 +18,6 @@ async function authenticate({ username, password }) {
     if (!user || !(await bcrypt.compare(password, user.hash)))
         throw 'Username or password is incorrect';
 
-    // authentication successful
     const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
     return { ...omitHash(user.get()), token };
 }
@@ -32,35 +31,29 @@ async function getById(id) {
 }
 
 async function create(params) {
-    // validate
     if (await db.User.findOne({ where: { username: params.username } })) {
         throw 'Username "' + params.username + '" is already taken';
     }
 
-    // hash password
     if (params.password) {
         params.hash = await bcrypt.hash(params.password, 10);
     }
 
-    // save user
     await db.User.create(params);
 }
 
 async function update(id, params) {
     const user = await getUser(id);
 
-    // validate
     const usernameChanged = params.username && user.username !== params.username;
     if (usernameChanged && await db.User.findOne({ where: { username: params.username } })) {
         throw 'Username "' + params.username + '" is already taken';
     }
 
-    // hash password if it was entered
     if (params.password) {
         params.hash = await bcrypt.hash(params.password, 10);
     }
 
-    // copy params to user and save
     Object.assign(user, params);
     await user.save();
 
@@ -72,7 +65,7 @@ async function _delete(id) {
     await user.destroy();
 }
 
-// helper functions
+
 
 async function getUser(id) {
     const user = await db.User.findByPk(id);
